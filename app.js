087e7640ab0e9b1356aa00652dee90d52c8beede
VincentLabor <vlabor95@gmail.com>
2019-06-08 23:10:53 -0700

    Added passport and removed salting

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const bodyparser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
 
 const app = express();
 
@@ -52,41 +55,43 @@ app.get("/register", function (req, res) {
 
 //The user submits a registration including their email and password
 app.post("/register", function (req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        let newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
+    // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    //     let newUser = new User({
+    //         email: req.body.username,
+    //         password: hash
+    //     });
 
-        newUser.save(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        })
-    });
+    //     newUser.save(function (err) {
+    //         if (err) {
+    //             console.log(err);
+    //         } else {
+    //             res.render("secrets");
+    //         }
+    //     })
+    // });
+
+
 });
 
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
+    // const username = req.body.username;
+    // const password = req.body.password;
 
-    User.findOne({
-        email: username
-    }, function (err, foundUsername) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUsername) { //This checks if there is a username actually found. 
-                bcrypt.compare(password, foundUsername.password, function (err, result) {
-                    if (result === true) {
-                        res.render("secrets");
-                    }
-                });
-            }
-        }
-    });
+    // User.findOne({
+    //     email: username
+    // }, function (err, foundUsername) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         if (foundUsername) { //This checks if there is a username actually found. 
+    //             bcrypt.compare(password, foundUsername.password, function (err, result) {
+    //                 if (result === true) {
+    //                     res.render("secrets");
+    //                 }
+    //             });
+    //         }
+    //     }
+    // });
 });
 
 app.listen(3000, function () {
