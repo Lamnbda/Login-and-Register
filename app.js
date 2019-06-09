80757e22e0ee31679e79bb027270b70c5553ead3
VincentLabor <vlabor95@gmail.com>
2019-06-08 18:02:29 -0700

    dotenv file hidden

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyparser = require('body-parser');
 const ejs = require('ejs');
@@ -6,6 +7,8 @@ const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -29,8 +32,8 @@ let userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret , encryptedFields: ['password'] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
 
 var User = mongoose.model('User', userSchema);
 
